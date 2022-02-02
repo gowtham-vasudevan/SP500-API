ef0e29165daafa0ea08889f584738ef03cecd1b7
Gowtham <96467275+gowtham-vasudevan@users.noreply.github.com>
2022-02-02 22:12:25 +0530

    Cleaner version with functions

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+# Installing the mysql connector
+pip install mysql-connector-python-rf
+
+# Importing the required libraries
 import requests
 import time
+import numpy as np
 import pandas as pd
+import mysql.connector
 
-
+# Alpha Vantage API key
 alpha_vantage_api_key = 'BQD3V3F1RBV81E53'
 
+# Reading text file containing symbols of S&P 500 companies
 with open('S&P500.txt') as f:
     companies = f.read().splitlines()
 
-
+#Creating pandas dataframe for each database to alter add to mysql database
 overview_dataframe = pd.DataFrame(columns=['Symbol', 'Name', 'Sector', 'Industry', 'Market Cap', 'Profit Margin', 'PE Ratio', 'Book Value', 'Dividend Per Share', 'EPS'])
 
 all_stocks = {}
@@ -16,38 +23,75 @@ for i in companies:
      all_stocks["{}".format(i)]=pd.DataFrame(columns=['Id', 'Dates', 'Symbol', 'Open', 'High', 'Low', 'Close', 'Volume'])
 
 
-for symbol in companies:
-    try:
-        response = requests.get(f'https://www.alphavantage.co/query?function=OVERVIEW&symbol={symbol}&apikey={alpha_vantage_api_key}').json()
-        if len(response) == 0:
-            print(f'No data to append to the DataFrame for {symbol}.')
-            time.sleep(30)
-            continue
-        print(f'Preparing {symbol} data to add into dataframe.')
-        row = {'Symbol': response['Symbol'], 'Name': response['Name'], 'Sector': response['Sector'], 'Industry': response['Industry'],
-           'Market Cap': response['MarketCapitalization'], 'Profit Margin': response['ProfitMargin'], 'PE Ratio': response['PERatio'],
-           'Book Value': response['BookValue'], 'Dividend Per Share': response['DividendPerShare'], 'EPS': response['EPS']}
-        overview_dataframe = overview_dataframe.append(row, ignore_index=True)
-        print(f'Added data for {symbol} to the dataframe.')
-        time.sleep(30)
-    except:
-        print(f'Error getting/adding data for {symbol}.')
-        time.sleep(30)
+# Database details
+database_name  = 'stockdatabase'
+passwd = 'apiproject'
+hostname = 'stockdatabase.cfsievxwcano.us-east-1.rds.amazonaws.com'
+port = 3306
+user_name = 'admin'
+
+# Connect to the databse
+try:
+    mydb = mysql.connector.connect(host=hostname, user=user_name,password=passwd)
+    cursor = mydb.cursor(buffered=True)
+except:
+    print("Can't Establish connection to the Companies Database.")
+    
+    
+# Function to add data to the dataframe
+def add_to_overview_dataframe(res):
+    row = {'Symbol': res['Symbol'], 'Name': res['Name'], 'Sector': res['Sector'], 'Industry': res['Industry'],
+        'Market Cap': res['MarketCapitalization'], 'Profit Margin': res['ProfitMargin'], 'PE Ratio': res['PERatio'],
+        'Book Value': res['BookValue'], 'Dividend Per Share': res['DividendPerShare'], 'EPS': res['EPS']}
+    overview_dataframe = overview_dataframe.append(row, ignore_index=True)
+    
+    
+# Functin to create mysql database using mysql connector
+def create_overview_database():
+    cursor.execute('USE Companies;')
+
+    cursor.execute("""CREATE TABLE Overview(
+            Symbol VARCHAR(7) PRIMARY KEY,
+            Name VARCHAR(30),
+            Sector VARCHAR(30),
+            Industry VARCHAR(30),
+            MarketCapitalisation FLOAT(2),
+            ProfitMargin FLOAT(2),
+            PERatio FLOAT(2),
+            BookValue FLOAT(2),
+            DividendPerShare FLOAT(2),
+            EPS FLOAT(2)
+        );""")
+    
+    
+# Insert the values into the database
+def insert_into_overview_database(df):
+    cursor.execute('USE Companies;')
+    for i,j in df.iterrows():
+        cursor.execute(f"""INSERT INTO Overview VALUES('{str(j[0])}', '{str(j[1])}', '{str(j[2])}', '{str(j[3])}', {float(j[4])}, {float(j[5])}, {float(j[6])}, {float(j[7])}, {float(j[8])}, {float(j[9])});""")
         
-        
-   for symbol in all_stocks.keys():
+
+for symbol in companies[0:499]:
     try:
-        response = requests.get(f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={alpha_vantage_api_key}').json()
+        response = requests.get(f'https://www.alphavantage.co/query?function=OVERVIEW&symbol={sym}&apikey={alpha_vantage_api_key}').json()
         if len(response) == 0:
-            print(f'No data to get from the {symbol} API.')
-            time.sleep(30)
-            continue
-        print(f'Preparing to ppend DataFrame for {symbol}.')
-        for date in response['Time Series (Daily)']:
-            row = {'Id':1, 'Dates': date, 'Symbol':symbol, 'Open': response['Time Series (Daily)'][str(date)]['1. open'], 'High': response['Time Series (Daily)'][str(date)]['2. high'], 'Low': response['Time Series (Daily)'][str(date)]['3. low'], 'Close': response['Time Series (Daily)'][str(date)]['4. close'], 'Volume': response['Time Series (Daily)'][str(date)]['5. volume']}
-            all_stocks[symbol] = all_stocks[symbol].append(row, ignore_index=True)
-            print(f'Added data for {symbol} to the dataframe.')
-            time.sleep(30)
+            print(f'Request returned empty for {symbol}.')
+            time.sleep(15)
+        else:
+            print(f'Successfull request for {symbol}.')
+            add_to_overview_dataframe(response)
+            print(f'Data appended to Overview Dataframe for {symbol}.')
+            time.sleep(15)
     except:
-        print(f'Error getting/adding data for {symbol}')
-        time.sleep(30)
+        print(f'Error requesting data for {symbol}.')
+        time.sleep(15)
+
+
+
+try:
+    insert_into_overview_database(overview_dataframe)
+except:
+    print('Error addinng data to the Overview Database.')
+    
+
+    
